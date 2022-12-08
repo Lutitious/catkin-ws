7fddf0c05918a013f67ab1f4ccea38c1a6c12813
Rahul Imran <notlutitious@gmail.com>
2022-12-08 11:56:46 +0000

    saddssda

--- build/CTestTestfile.cmake
+++ build/CTestTestfile.cmake
@@ -1,6 +1,6 @@
 # CMake generated Testfile for 
-# Source directory: /home/rahul/catkin_ws/src
-# Build directory: /home/rahul/catkin_ws/build
+# Source directory: /home/rahul/catkin-ws/src
+# Build directory: /home/rahul/catkin-ws/build
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -7,4 +7,4 @@
 # new environment variables
 
 # modified environment variables
-export PWD='/home/rahul/catkin_ws/build'
+export PWD='/home/rahul/catkin-ws/build'
